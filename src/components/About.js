892b9aed3ca8894e4a98029b37264a5b811c8d21
Tushar Aggarwal <aggtushar123@gmail.com>
2023-07-07 18:27:14 +0530

    Frontend Initial Add

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React, {useContext, useEffect} from 'react'
+import noteContext from '../context/Notes/noteContext'
+
+const About = () => {
+    const a = useContext(noteContext)
+    useEffect(()=>{
+        a.update();
+        // eslint-disable-next-line
+    },[])
+  return (
+    <div>
+      This is About {a.state.name}
+    </div>
+  )
+}
+
+export default About
